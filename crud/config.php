acc750cd8b1f1d9390f3ea8211e682752c1d4055
iqrakhan22 <iqrakhan22@email.com>
2024-09-08 21:39:57 -0700

    oop

--- crud/config.php
+++ crud/config.php
@@ -3,11 +3,11 @@ $host = "localhost";
 $username ="root";
 $password = "";
 $dbname = "db_std";
-$con = new mysqli($host, $username, $password, $dbname);
-if($con){
-    echo "database connected";
-}else{
-    echo "database not connected";
-}
+//$con = new mysqli($host, $username, $password, $dbname);
+// if($con){
+//     echo "database connected";
+// }else{
+//     echo "database not connected";
+// }
 
 ?>
